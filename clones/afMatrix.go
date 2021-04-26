3cc1b54280628a2fdb931a1fb524a705bcf75533
Daniel <daniel.snellings@duke.edu>
2021-04-26 16:00:41 -0400

    afmatrix

---
+++ clones/afMatrix.go
@@ -0,0 +1,41 @@
+package clones
+
+import (
+	"github.com/ddsnellings/weaver/cells"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
+	"log"
+)
+
+func PrincipalComponent(d *cells.Data) stat.PC {
+	var answer stat.PC
+	ok := answer.PrincipalComponents(generateAfMatrix(d), nil)
+	if !ok {
+		log.Panic("problem with PC analysis")
+	}
+	return answer
+}
+
+// generateAfMatrix creates a matrix of allele frequencies where each column is variant and each row is a cell.
+// The allele frequency for missing genotypes is set to -50.
+func generateAfMatrix(d *cells.Data) *mat.Dense {
+	afSlice := fetchAfFromData(d)
+	return mat.NewDense(len(d.Cells), len(d.Variants), afSlice)
+}
+
+// fetchAfFromData retrieves a slice of allele frequencies for each variant in each cell. Missing genotypes
+// are set to -50. The slice is ordered by cell then by variant
+// e.g. {C1_VAF1, C1_VAF2, C1_VAF3, C2_VAF1, C2_VAF2, C2_VAF3, ... Cn_VAF3}
+func fetchAfFromData(d *cells.Data) []float64 {
+	answer := make([]float64, len(d.Cells) * len(d.Variants))
+	for i := range d.Cells {
+		for j := range d.Cells[i].Genotypes {
+			if d.Cells[i].Genotypes[j].Genotype == cells.NoGenotype {
+				answer[i*j] = -50
+			} else {
+				answer[i*j] = d.Cells[i].Genotypes[j].Af
+			}
+		}
+	}
+	return answer
+}
---
+++ clones/afMatrix_test.go
@@ -0,0 +1,76 @@
+package clones
+
+import (
+	"fmt"
+	"github.com/ddsnellings/weaver/cells"
+	"github.com/vertgenlab/gonomics/fileio"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
+	"log"
+	"strings"
+	"testing"
+)
+
+var defaultVcfQual float64 = 100
+var defaultCellFilter = cells.CellFilterParam{MinGenotypeQuality: 30, MinGenotypeDepth: 10, MinReadAf: 0.2}
+var defaultGlobalFilter = cells.GlobalFilterParam{MinGenotypedFrac: 0.5, MinGenotypesPresent: 0.5, MinCellAf: 0.01}
+
+var v1file = "/Users/danielsnellings/Desktop/Data/21-04-06_Tapestri_Run/CM2001_R/CM2001.vcf.gz"
+var v2file = "/Users/danielsnellings/Desktop/Data/21-04-06_Tapestri_Run/V2_Analysis/CM2001_2.vcf.gz"
+var testfile = "../cells/testdata/small.vcf"
+
+func TestPC(t *testing.T) {
+	d := cells.ReadVcf(v1file, defaultCellFilter, defaultGlobalFilter, defaultVcfQual)
+	afMat := generateAfMatrix(d)
+	var pc stat.PC
+	pc.PrincipalComponents(afMat, nil)
+
+	fmt.Printf("variances = %.4f\n\n", pc.VarsTo(nil))
+
+	// Project the data onto the first 2 principal components.
+	k := 2
+	var proj mat.Dense
+	var vec mat.Dense
+	pc.VectorsTo(&vec)
+	proj.Mul(afMat, vec.Slice(0, len(d.Variants), 0, k))
+
+
+	out := fileio.EasyCreate("pc.csv")
+	_, err := fmt.Fprintln(out, getHeader(d))
+	if err != nil {
+		log.Panic()
+	}
+	var vals []float64
+	rows, _ := proj.Dims()
+	for i := 0; i < rows; i++ {
+		vals = proj.RawRowView(i)
+		_, err = fmt.Fprintf(out, "%.4f,%.4f%s\n", vals[0], vals[1], getAfStringCSV(i, afMat))
+		if err != nil {
+			log.Panic()
+		}
+	}
+	err = out.Close()
+	if err != nil {log.Panic()}
+
+	//fmt.Printf("proj = %.4f", mat.Formatted(&proj, mat.Prefix("       ")))
+}
+
+func getHeader(d *cells.Data) string {
+	var answer strings.Builder
+	answer.Grow(len(d.Variants) * 20)
+	answer.WriteString("pc1,pc2")
+	for i := range d.Variants {
+		answer.WriteString(fmt.Sprintf(",%s", d.Variants[i]))
+	}
+	return answer.String()
+}
+
+func getAfStringCSV(i int, m *mat.Dense) string {
+	var answer strings.Builder
+	vals := m.RawRowView(i)
+	answer.Grow(len(vals) * 5)
+	for _, val := range vals {
+		answer.WriteString(fmt.Sprintf(",%.4f", val))
+	}
+	return answer.String()
+}
