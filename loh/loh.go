6c2373d95f0bca8f5d57c2d82eccb46cebee224e
Daniel <daniel.snellings@duke.edu>
2021-04-30 12:48:38 -0400

    haplotype symmetry

--- loh/loh.go
+++ loh/loh.go
@@ -9,6 +9,7 @@
 package loh
 
 import (
+	"fmt"
 	"github.com/ddsnellings/weaver/cells"
 	"github.com/ddsnellings/weaver/variants"
 )
@@ -27,17 +28,58 @@ func (r RunOfHomozygosity) Region(v []variants.Variant) variants.Region {
 	}
 }
 
+// Haplotype stores the variant and zygosity information for a set of variants
+type Haplotype struct {
+	VariantIds []int
+	Genotypes []variants.Zygosity
+}
+
+func (h Haplotype) String() string {
+	return variantIdsString(h.VariantIds) + ":" + genotypesToString(h.Genotypes)
+}
+
+func variantIdsString(ids []int) string {
+	if len(ids) == 0 {
+		return ""
+	}
+	answer := fmt.Sprintf("%d", ids[0])
+	for i := 1; i < len(ids); i++ {
+		answer += fmt.Sprintf("-%d", ids[i])
+	}
+	return answer
+}
+
+func genotypesToString(z []variants.Zygosity) string {
+	if len(z) == 0 {
+		return ""
+	}
+	answer := fmt.Sprint(z[0])
+	for i := 1; i < len(z); i++ {
+		answer += fmt.Sprint("-", z[i])
+	}
+	return answer
+}
+
+// RohHaplotypes stores the unique haplotypes present in a cells with a shared region.
+// e.g. Haplotype1: WT-WT-WT Haplotype2: Hom-Hom-Hom Haplotype3: Hom-WT-Hom Haplotype4: Hom-NA-Hom
+// A count of cells is stored such that HaplotypeCounts[i] is the number of cells with Haplotype[i].
+type RohHaplotypes struct {
+	Haplotypes			 	[]Haplotype
+	HaplotypeCounts 		[]int
+}
+
 // FindAllRunsOfHomozygosity identifies constitutionally heterozygous variants go to
 // homozygosity across a contiguous genomic span. Returns a slice of slices of
 // RunOfHomozygosity where return[i] is a slice of all homozygous runs for cell
-// with Id == i.
-func FindAllRunsOfHomozygosity(d *cells.Data) [][]RunOfHomozygosity {
+// with Id == i. A putative ROH is only considered after is defined by > minVars
+// of variants. i.e. an ROH defined by 2 SNPs is not returned if minVars == 3
+func FindAllRunsOfHomozygosity(d *cells.Data, minVars int) [][]RunOfHomozygosity {
 	answer := make([][]RunOfHomozygosity, len(d.Cells))
 	hetVariantIds := variants.FindHeterozygous(d.Variants)
 	variants.SortIdsByCoord(hetVariantIds, d.Variants)
 
 	for i := range d.Cells {
-		answer[i] = FindRunsOfHomozygosity(d.Cells[i], hetVariantIds, d.Variants)
+		answer[i] = FindRunsOfHomozygosity(d.Cells[i], hetVariantIds, d.Variants, minVars)
 	}
 	return answer
 }
@@ -45,15 +87,17 @@ func FindAllRunsOfHomozygosity(d *cells.Data) [][]RunOfHomozygosity {
 // FindRunsOfHomozygosity identifies constitutionally heterozygous variants go to
 // homozygosity across a contiguous genomic span in the input cell.
 // The hetVariantIds input should be a slice of constitutional heterozygous variants
-// sorted by genomic coordinate with variants.SortIdsByCoord.
-func FindRunsOfHomozygosity(c cells.Cell, hetVariantIds []int, vars []variants.Variant) []RunOfHomozygosity {
+// sorted by genomic coordinate with variants.SortIdsByCoord. A putative ROH is only
+// considered after is defined by > minVars of variants.
+// i.e. an ROH defined by 2 SNPs is not returned if minVars == 3
+func FindRunsOfHomozygosity(c cells.Cell, hetVariantIds []int, vars []variants.Variant, minVars int) []RunOfHomozygosity {
 	var answer []RunOfHomozygosity
 	var currRun RunOfHomozygosity
 	for i := range hetVariantIds {
 
 		// do not let ROH span different chromosomes
 		if i > 0 && vars[hetVariantIds[i]].Chr != vars[hetVariantIds[i-1]].Chr {
-			if len(currRun) > 1 {
+			if len(currRun) >= minVars {
 				answer = append(answer, currRun)
 			}
 			currRun = nil
@@ -61,7 +105,7 @@ func FindRunsOfHomozygosity(c cells.Cell, hetVariantIds []int, vars []variants.V
 
 		switch c.Genotypes[hetVariantIds[i]].Genotype {
 		case variants.Heterozygous:
-			if len(currRun) > 1 {
+			if len(currRun) >= minVars {
 				answer = append(answer, currRun)
 			}
 			currRun = nil
@@ -75,18 +119,71 @@ func FindRunsOfHomozygosity(c cells.Cell, hetVariantIds []int, vars []variants.V
 		}
 	}
 
-	if len(currRun) > 1 { // in case we end in an ongoing run
+	if len(currRun) >= minVars { // in case we end in an ongoing run
 		answer = append(answer, currRun)
 	}
 	return answer
 }
 
-func CountRunsOfHomozygosity(r [][]RunOfHomozygosity, v []variants.Variant) map[variants.Region]int {
-	answer := make(map[variants.Region]int)
+// CountRohHaplotypes determines the number of cells harboring each unique run of homozygosity.
+// The return map is keyed on the genomic region spanned by the ROH and give the number of cells with
+// ROH across the keyed region.
+func CountRohHaplotypes(r [][]RunOfHomozygosity, d *cells.Data) map[variants.Region]*RohHaplotypes {
+	answer := make(map[variants.Region]*RohHaplotypes)
 	for i := range r {
 		for _, run := range r[i] {
-			answer[run.Region(v)]++
+			addHaplotype(i, run, answer, d)
 		}
 	}
 	return answer
 }
+
+// addHaplotype determines the haplotype zygosity for the input run of homozygosity and fills the answer map accordingly.
+func addHaplotype(cellId int, run RunOfHomozygosity, answer map[variants.Region]*RohHaplotypes, d *cells.Data) {
+	var haplotype Haplotype
+	haplotype.VariantIds = run
+	haplotype.Genotypes = make([]variants.Zygosity, len(run))
+	for i := range run {
+		haplotype.Genotypes[i] = d.Cells[cellId].Genotypes[run[i]].Genotype
+	}
+
+	var rohHap *RohHaplotypes
+	var ok bool
+	if rohHap, ok = answer[run.Region(d.Variants)]; !ok {
+		answer[run.Region(d.Variants)] = &RohHaplotypes{}
+		rohHap = answer[run.Region(d.Variants)]
+	}
+
+	idx, found := findHaplotypeIdx(haplotype, rohHap.Haplotypes)
+
+	if found {
+		rohHap.HaplotypeCounts[idx]++
+	} else {
+		rohHap.Haplotypes = append(rohHap.Haplotypes, haplotype)
+		rohHap.HaplotypeCounts = append(rohHap.HaplotypeCounts, 1)
+	}
+}
+
+func findHaplotypeIdx(val Haplotype, searchHaps []Haplotype) (idx int, found bool) {
+	for i := range searchHaps {
+		if matchingHaplotype(val, searchHaps[i]) {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+// matchingHaplotype returns true if a and b have identical haplotype zygosity.
+func matchingHaplotype(a, b Haplotype) bool {
+	if len(a.Genotypes) != len(b.Genotypes) {
+		return false
+	}
+	for i := range a.Genotypes {
+		if a.Genotypes[i] != b.Genotypes[i] || a.VariantIds[i] != b.VariantIds[i] {
+			return false
+		}
+	}
+	return true
+}
+
+
--- loh/loh_test.go
+++ loh/loh_test.go
@@ -3,8 +3,6 @@ package loh
 import (
 	"fmt"
 	"github.com/ddsnellings/weaver/cells"
-	"github.com/vertgenlab/gonomics/fileio"
-	"log"
 	"testing"
 )
 
@@ -14,27 +12,52 @@ var testfile = "../cells/testdata/small.vcf"
 
 func TestFindRoh(t *testing.T) {
 	d := cells.ReadVcf(v1file, cells.DefaultCellFilter, cells.DefaultGlobalFilter, cells.DefaultVcfQual)
-	roh := FindAllRunsOfHomozygosity(d)
 
-	counts := CountRunsOfHomozygosity(roh, d.Variants)
+	//roh2 := FindAllRunsOfHomozygosity(d, 2)
+	//roh3 := FindAllRunsOfHomozygosity(d, 3)
+	//roh4 := FindAllRunsOfHomozygosity(d, 4)
+	roh5 := FindAllRunsOfHomozygosity(d, 5)
 
-	fmt.Println("Length\tCount")
-	for key, val := range counts {
-		fmt.Println(key.End-key.Start, "\t", val)
+	//var found bool
+	//for i := range roh5 {
+	//	for _, j := range roh5[i] {
+	//		if j.Region(d.Variants).End == 169237430 {
+	//			for _, id := range j {
+	//				fmt.Println(d.Variants[id])
+	//			}
+	//			found = true
+	//			break
+	//		}
+	//	}
+	//	if found {
+	//		break
+	//	}
+	//}
+
+	//counts2 := CountRohHaplotypes(roh2, d)
+	//counts3 := CountRohHaplotypes(roh3, d)
+	//counts4 := CountRohHaplotypes(roh4, d)
+	counts5 := CountRohHaplotypes(roh5, d)
+
+	fmt.Println("Chr\tStart\tEnd\tCount_min5")
+	for key, val := range counts5 {
+		for i := range val.Haplotypes {
+			fmt.Printf("%s\t%d\t%d\t%s\t%d\n", key.Chr, key.Start, key.End, val.Haplotypes[i], val.HaplotypeCounts[i])
+		}
 	}
 
-
+	/*
 	out := fileio.EasyCreate("roh.csv")
 	_, err := fmt.Fprintln(out, "Cell,Chromosome,StartOrEnd,Pos")
 	if err != nil {
 		log.Panic()
 	}
 
-	for i := range roh {
-		if len(roh[i]) == 0 {
+	for i := range roh5 {
+		if len(roh5[i]) == 0 {
 			continue
 		}
-		for _, run := range roh[i] {
+		for _, run := range roh5[i] {
 			_, err = fmt.Fprintf(out, "Cell_%d,%s,%s,%d\n", i, d.Variants[run[0]].Chr, "start", d.Variants[run[0]].Pos+1)
 			if err != nil {
 				log.Panic()
@@ -50,4 +73,6 @@ func TestFindRoh(t *testing.T) {
 	if err != nil {
 		log.Panic()
 	}
+
+ */
 }
